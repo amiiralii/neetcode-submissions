dfb14de8c31f31f7f37197bf5758fe669ffaa87e
Amirali Rayegan <54767883+amiiralii@users.noreply.github.com>
2026-09-16 21:19:14 -0400

    Add: valid-sudoku - submission-6

---
+++ Data Structures & Algorithms/valid-sudoku/submission-6.py
@@ -0,0 +1,39 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        def col_extractor(board):
+            for i in range(len(board)):
+                yield [row[i] for row in board]
+        
+        def sq_extractor(board):
+            for i in range(0, len(board), 3):
+                sq, sq1, sq2 = [],[],[]
+                for row in board[i:i+3]:
+                    sq.append(row[0])
+                    sq.append(row[1])
+                    sq.append(row[2])
+
+                    sq1.append(row[3])
+                    sq1.append(row[4])
+                    sq1.append(row[5])
+
+                    sq2.append(row[6])
+                    sq2.append(row[7])
+                    sq2.append(row[8])
+                yield sq
+                yield sq1
+                yield sq2
+
+        digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        for d in digits:
+            for row in board:
+                if row.count(d) > 1:
+                    return False
+            for col in col_extractor(board):
+                if col.count(d) > 1:
+                    return False
+            for sq in sq_extractor(board):
+                if sq.count(d) > 1:
+                    return False
+        return True
+
+        # Run time = 9 * (9 + 9*9 + 9*9) = 81 + 729 + 729 = 1539
